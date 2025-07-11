982613b9895a5d07f8be188cea528305519ef363
g4mer9 <rileyfinkgamer9@gmail.com>
2025-07-11 10:39:01 -0700

    first shitty strategy implemented

--- P3/behavior_tree_bot/behaviors.py
+++ P3/behavior_tree_bot/behaviors.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, '../')
 from planet_wars import issue_order
+from random import choice
 
 
 def attack_weakest_enemy_planet(state):
@@ -20,7 +21,16 @@ def attack_weakest_enemy_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+    
+def production_attempt(state):
+    
+    my_planet = choice(state.my_planets())
+    #grab distance using distance(planet1, planet2) function instead of distance_to
+    closest_planet = min(state.neutral_planets(), key=lambda p: state.distance(state.my_planets().index(my_planet), state.neutral_planets().index(p)), default=None)
+    if closest_planet and closest_planet.num_ships < 40:
+        return issue_order(state, my_planet.ID, closest_planet.ID, 40 - closest_planet.num_ships)
 
+    return False
 
 def spread_to_weakest_neutral_planet(state):
     # (1) If we currently have a fleet in flight, just do nothing.
--- P3/behavior_tree_bot/bt_bot.py
+++ P3/behavior_tree_bot/bt_bot.py
@@ -26,6 +26,11 @@ def setup_behavior_tree():
     # Top-down construction of behavior tree
     root = Selector(name='High Level Ordering of Strategies')
 
+    production_plan = Sequence(name='Production Strategy')
+    production_check = Check(if_neutral_planet_available)
+    production_action = Action(production_attempt)
+    production_plan.child_nodes = [production_check, production_action]
+
     offensive_plan = Sequence(name='Offensive Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_weakest_enemy_planet)
@@ -36,7 +41,7 @@ def setup_behavior_tree():
     spread_action = Action(spread_to_weakest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    root.child_nodes = [production_plan, offensive_plan, spread_sequence, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
