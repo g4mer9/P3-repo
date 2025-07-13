e76b9e58dffb32cfb181ca73c4e2b7b62ff12a48
g4mer9 <rileyfinkgamer9@gmail.com>
2025-07-13 10:04:48 -0700

    added comments

--- P3/behavior_tree_bot/behaviors.py
+++ P3/behavior_tree_bot/behaviors.py
@@ -25,18 +25,25 @@ def attack_weakest_enemy_planet(state):
     
 def production_attempt(state):
     
+    # looping over all planets to find the first one that has enough ships to send and is close enough to the target planet
     for my_planet in state.my_planets():
     
+        # grab all target planets that are not currently targeted by my fleets
+        # and sort them by number of ships
         target_planets = [planet for planet in state.not_my_planets()
                         if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
         target_planets = sorted(target_planets, key=lambda p: p.num_ships, reverse=False)
 
         if target_planets:
+            # target the weakest planet
             target_planet = target_planets[0]
             if(target_planet.owner == 0):
+                # if the target planet is neutral, we can send any number of ships
                 required_ships = target_planet.num_ships + 1
             else:
+                # if the target planet is owned by an enemy, we need to send enough ships to conquer it
                 required_ships = target_planet.num_ships + state.distance(my_planet.ID, target_planet.ID) * target_planet.growth_rate + 1
+            # if we have enough ships to send, issue the order
             if my_planet.num_ships > required_ships:
                 return issue_order(state, my_planet.ID, target_planet.ID, required_ships)
     return False
